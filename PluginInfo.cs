6ea4ae3ae9a468cf99cf1dc17443501c093f1d76
spooderman17 <j5comer@outlook.com>
2026-05-03 16:19:16 -0400

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace StupidTemplate
+{
+    public class PluginInfo
+    {
+        public const string GUID = "org.iidk.gorillatag.menutemplate";
+        public const string Name = "ii's Stupid Template";
+        public const string Description = "Created by @goldentrophy with love <3";
+        public const string Version = "1.9.8";
+    }
+}
